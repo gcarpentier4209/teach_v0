93ae29d322cd480cb8de153d210eab9d98286cbc
yannik <yguillodo@gmail.com>
2019-02-18 21:10:09 +0100

    essai avec vue.js

---
+++ resources/js/components/sidebarForm.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <form class="flex-column">
+        <textarea class="border rounded p-3"></textarea>
+        <button type ="submit" class="border rounded py-3"> voir </button>
+
+
+    </form>
+</template>
+
+<script>
+
+    export default {
+
+
+    }
+
+
+</script>
